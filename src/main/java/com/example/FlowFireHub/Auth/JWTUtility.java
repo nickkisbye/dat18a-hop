b075c45da3fa65e8ba6b4f1273211a731c51d7d8
nickkisbye <nick4630@stud.kea.dk>
2020-11-30 17:10:04 +0100

    User and AuthController setup med bcrypt

--- src/main/java/com/example/FlowFireHub/Auth/JWTUtility.java
+++ src/main/java/com/example/FlowFireHub/Auth/JWTUtility.java
@@ -21,8 +21,7 @@ public class JWTUtility implements Serializable {
 
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-    @Value("${jwt.secret}")
-    private String secret;
+    private String secret = "test";
 
     //retrieve username from jwt token
     public String getUsernameFromToken(String token) {

a197eeae771022a9177b2c7b14049a4dbf9f7f99
rasm651 <rasm651v@stud.kea.dk>
2021-01-16 13:26:22 +0100

    merging

--- src/main/java/com/example/FlowFireHub/Controllers/UserController.java
+++ src/main/java/com/example/FlowFireHub/Controllers/UserController.java
@@ -39,4 +39,10 @@ public class UserController implements Serializable {
         }
     }
 
+    @GetMapping("/test")
+    public Iterable<User> test() {
+        Iterable<User> users = userRepository.findAll();
+        return users;
+    }
+
 }
